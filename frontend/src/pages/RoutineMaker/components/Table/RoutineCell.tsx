c8afdbc98da4f07e581d9a957afd040a3a086b10
Adalberto <adalberto.devs@gmail.com>
2022-05-08 15:14:22 +0100

    Download image bad cented text fixed

--- frontend/src/pages/RoutineMaker/components/Table/RoutineCell.tsx
+++ frontend/src/pages/RoutineMaker/components/Table/RoutineCell.tsx
@@ -1,5 +1,12 @@
 import { Autocomplete } from '@mui/material';
-import { useContext, useRef } from 'react';
+import {
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef
+} from 'react';
 import Exercise from '../../../../models/Exercise';
 import {
   StyledCellGrid,
@@ -9,110 +16,123 @@ import {
 import { TableContext } from '../../contexts/TableContext';
 import { Cell } from '../../models/Cell';
 
-const RoutineCell: React.FC<Cell> = ({
-  exercise,
-  series,
-  repetitions,
-  weight,
-  cell
-}) => {
-  const { table, dispatch } = useContext(TableContext);
-  const { exercises } = table;
-  const row = cell?.row || 0;
-  const column = cell?.column || 0;
+const RoutineCell: React.FC<Cell> = memo(
+  ({ exercise, series, repetitions, weight, cell }) => {
+    const { table, dispatch } = useContext(TableContext);
+    const { exercises } = table;
+    const row = cell?.row || 0;
+    const column = cell?.column || 0;
 
-  const exerciseInput = useRef<HTMLInputElement>(null);
+    const exerciseInput = useRef<HTMLInputElement>(null);
 
-  const handleAutocompleteChange = (
-    e: React.SyntheticEvent<Element, Event>,
-    value: string | Exercise
-  ) => {
-    if (exerciseInput.current) {
-      exerciseInput.current.setAttribute('value', (value as Exercise).label);
-      let evt = new Event('input', { bubbles: true });
-      exerciseInput.current.dispatchEvent(evt);
-    }
-  };
+    const handleAutocompleteChange = (
+      e: React.SyntheticEvent<Element, Event>,
+      value: string | Exercise
+    ) => {
+      if (exerciseInput.current) {
+        exerciseInput.current.setAttribute('value', (value as Exercise).label);
+        let evt = new Event('input', { bubbles: true });
+        exerciseInput.current.dispatchEvent(evt);
+      }
+    };
 
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    dispatch({
-      type: 'changeInput',
-      payload: { name: e.target.name, value: e.target.value }
-    });
-  };
+    const handleInputChange = (
+      e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    ) => {
+      dispatch({
+        type: 'changeInput',
+        payload: { name: e.target.name, value: e.target.value }
+      });
+    };
 
-  const colorByMuscle = new Map([
-    ['Pectoral', '#99ff66'],
-    ['Hombros', '#ccff00'],
-    ['Tríceps', '#66ffff'],
-    ['Espalda', '#00ccff'],
-    ['Bíceps', '#33ff99'],
-    ['Cuádriceps', '#6666ff'],
-    ['Glúteos', '#9900ff'],
-    ['Femoral', '#ff33ff'],
-    ['Gemelos', '#ff00cc']
-  ]);
+    const colorByMuscle = useMemo(
+      () =>
+        new Map([
+          ['Pectoral', '#99ff66'],
+          ['Hombros', '#ccff00'],
+          ['Tríceps', '#66ffff'],
+          ['Espalda', '#00ccff'],
+          ['Bíceps', '#33ff99'],
+          ['Cuádriceps', '#6666ff'],
+          ['Glúteos', '#9900ff'],
+          ['Femoral', '#ff33ff'],
+          ['Gemelos', '#ff00cc']
+        ]),
+      []
+    );
 
-  const exerciseObject: Exercise =
-    exercises?.find((exercises) => exercises.label === exercise) ||
-    ({} as Exercise);
-  const background: string = colorByMuscle.get(exerciseObject.muscle) || '';
+    const styledCell = useRef<HTMLDivElement | null>(null);
 
-  return (
-    <TableCell>
-      <StyledCellGrid style={{ backgroundColor: background }}>
-        <Autocomplete
-          className='autocomplete'
-          disableClearable
-          freeSolo
-          selectOnFocus
-          options={exercises ? exercises : []}
-          groupBy={(exercise) => exercise.muscle}
-          inputValue={exercise}
-          onChange={handleAutocompleteChange}
-          renderInput={(params) => (
-            <StyledTextField
-              {...params}
-              InputProps={{
-                ...params.InputProps,
-                inputProps: {
-                  ...params.inputProps,
-                  style: { padding: 4 }
-                }
-              }}
-              name={`rows.${row}.columns.${column}.exercise`}
-              inputRef={exerciseInput}
-              variant='standard'
-              onChange={handleInputChange}
-            />
-          )}
-        />
+    const handleChangeInputColor = useCallback(() => {
+      if (styledCell.current) {
+        const exerciseObject: Exercise =
+          exercises?.find((exercises) => exercises.label === exercise) ||
+          ({} as Exercise);
+        const background: string =
+          colorByMuscle.get(exerciseObject.muscle) || '';
 
-        <StyledTextField
-          name={`rows.${row}.columns.${column}.series`}
-          variant='standard'
-          value={series}
-          onChange={handleInputChange}
-        />
+        styledCell.current.style.backgroundColor = background;
+      }
+    }, [colorByMuscle, exercise, exercises]);
 
-        <StyledTextField
-          name={`rows.${row}.columns.${column}.repetitions`}
-          variant='standard'
-          value={repetitions}
-          onChange={handleInputChange}
-        />
+    useEffect(() => {
+      handleChangeInputColor();
+    }, [handleChangeInputColor]);
 
-        <StyledTextField
-          name={`rows.${row}.columns.${column}.weight`}
-          variant='standard'
-          value={weight}
-          onChange={handleInputChange}
-        />
-      </StyledCellGrid>
-    </TableCell>
-  );
-};
+    return (
+      <TableCell>
+        <StyledCellGrid ref={styledCell}>
+          <Autocomplete
+            className='autocomplete'
+            disableClearable
+            freeSolo
+            selectOnFocus
+            options={exercises ? exercises : []}
+            groupBy={(exercise) => exercise.muscle}
+            inputValue={exercise}
+            onChange={handleAutocompleteChange}
+            renderInput={(params) => (
+              <StyledTextField
+                {...params}
+                InputProps={{
+                  ...params.InputProps,
+                  inputProps: {
+                    ...params.inputProps,
+                    style: { padding: 4 }
+                  }
+                }}
+                name={`rows.${row}.columns.${column}.exercise`}
+                inputRef={exerciseInput}
+                variant='standard'
+                onChange={handleInputChange}
+              />
+            )}
+          />
+
+          <StyledTextField
+            name={`rows.${row}.columns.${column}.series`}
+            variant='standard'
+            value={series}
+            onChange={handleInputChange}
+          />
+
+          <StyledTextField
+            name={`rows.${row}.columns.${column}.repetitions`}
+            variant='standard'
+            value={repetitions}
+            onChange={handleInputChange}
+          />
+
+          <StyledTextField
+            name={`rows.${row}.columns.${column}.weight`}
+            variant='standard'
+            value={weight}
+            onChange={handleInputChange}
+          />
+        </StyledCellGrid>
+      </TableCell>
+    );
+  }
+);
 
 export default RoutineCell;
--- frontend/src/pages/RoutineMaker/helpers/downloadHTML.ts
+++ frontend/src/pages/RoutineMaker/helpers/downloadHTML.ts
@@ -1,4 +1,5 @@
-import html2canvas from 'html2canvas';
+import { getFontEmbedCSS, toPng } from 'html-to-image-v2';
+import downloadjs from 'downloadjs';
 
 export const downloadHTML = async (element: HTMLElement | string | null) => {
   if (element === null) {
@@ -9,10 +10,17 @@ export const downloadHTML = async (element: HTMLElement | string | null) => {
     element = document.querySelector(`#${element}`) as HTMLElement;
   }
 
-  const canvas = await html2canvas(element);
-  const img = canvas.toDataURL('img/png');
-  const link = document.createElement('a');
-  link.download = 'Rutine.png';
-  link.href = img;
-  link.click();
+  try {
+    const fontEmbedCSS = await getFontEmbedCSS(element);
+
+    const dataUrl = await toPng(element, {
+      quality: 1,
+      backgroundColor: 'white',
+      fontEmbedCSS: fontEmbedCSS,
+      cacheBust: true
+    });
+    downloadjs(dataUrl, 'routine.png', 'image/png');
+  } catch (error) {
+    console.error('Error: ', error);
+  }
 };
