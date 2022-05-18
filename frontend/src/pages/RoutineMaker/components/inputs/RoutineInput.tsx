2799e6115d6dd43445d600f224c46345e0efe76e
Adalberto <adalberto.devs@gmail.com>
2022-05-18 16:03:01 +0100

    Autoresize font

--- frontend/src/pages/RoutineMaker/components/inputs/RoutineInput.tsx
+++ frontend/src/pages/RoutineMaker/components/inputs/RoutineInput.tsx
@@ -1,11 +1,21 @@
 import { StandardTextFieldProps } from '@mui/material';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { StyledTextField } from '../../styled-components/Table';
+import { AutoFitText } from '../../utils/AutoFitText';
 
-interface RoutineInputProps extends StandardTextFieldProps {
-  value?: string;
-}
+interface RoutineInputProps extends StandardTextFieldProps {}
 
 export const RoutineInput: React.FC<RoutineInputProps> = memo((params) => {
-  return <StyledTextField {...params} variant='standard' />;
+  const fontSize = useMemo(
+    () =>
+      AutoFitText({
+        maxSize: 15,
+        minSize: 10,
+        input: (params.inputRef as React.RefObject<HTMLInputElement>)?.current,
+        inputValue: params.InputProps?.inputProps?.value || ''
+      }),
+    [params.InputProps?.inputProps?.value, params.inputRef]
+  );
+
+  return <StyledTextField {...params} fontSize={fontSize} variant='standard' />;
 });
